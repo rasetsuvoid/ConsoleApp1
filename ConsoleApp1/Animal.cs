20c2fabee6df826d224445260223f37e86d36eb9
raset <raset@DESKTOP-2TGGUJA>
2026-01-29 18:52:34 -0500

    Agregar archivos de proyecto.

---
+++ ConsoleApp1/Animal.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+//ConsoleApp1.Domain
+//Console-app1.Domain
+// snake_case NO USARLO
+// CamelCase (ConsoleApp1)
+namespace ConsoleApp1
+{
+    // Clase Animal
+    // Modificador de acceso: public,
+    // private,
+    // protected,
+    // internal,
+    // protected internal,
+    // private protected
+    public class Animal
+    {
+        // Constructor por defecto
+        public Animal() { }
+
+        // Propiedad Nombre
+        // Tipo de dato: string
+        // Modificador de acceso: public
+        // Nombre de la propiedad: Nombre
+        // metodos accesores get y set
+        public string Nombre { get; set; }
+        //prop
+        public string Especie { get; set; }
+        public int Edad { get; set; }
+
+        // Metodo HacerSonido
+        // Modificador de acceso: public
+        // Tipo de retorno: void -> no retorna nada
+        // Nombre del metodo: HacerSonido
+        // Parametros: ninguno
+        public void HacerSonido()
+        {
+            Console.WriteLine("El animal hace un sonido.");
+        }
+
+        // Metodo ObtenerInformacion
+        // Modificador de acceso: public
+        // Tipo de retorno: string -> retorna una cadena de texto
+        public string ObtenerInformacion()
+        {
+            // Retorna la informacion del animal
+            return $"Nombre: {Nombre}, Especie: {Especie}, Edad: {Edad}";
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,62 @@
+﻿// Clase static Console -- WriteLine method (parametros)
+Console.WriteLine("Hello, World!");
+//Console.ReadLine();
+
+// if (true)
+if (1 > 2)
+{
+    // Clase static Console -- WriteLine method (parametros)
+    Console.WriteLine("This is true.");
+}
+else
+{
+
+}
+
+string ternario = 1 > 2 ? "True" : "False";
+
+if (1 > 3)
+{
+
+}else if (1 > 4)
+{
+
+}
+else
+{
+
+}
+
+
+for (int i = 0; i < 10; i++)
+{
+    // Clase static Console -- WriteLine method (parametros)
+    Console.WriteLine(i);
+}
+
+List<string> list = new List<string>();
+
+list.Add("Item 1");
+list.Add("Item 2");
+
+
+foreach (string item in list)
+{
+    Console.WriteLine(item);
+}
+
+// tipos de datos.
+string miCadena = "Hola Mundo";
+int longitud = 1;
+long longitudLarga = 10000000000;
+float numeroDecimal = 10.5f;
+decimal numeroDecimal2 = 20.5m;
+bool esVerdadero = true;
+char caracter = 'A';
+var numeroInferido = "sasd";
+var numeroInferid2 = 10;
+
+byte[] bytes = new byte[10];
+
+List<string> list2 = new List<string>();
+
